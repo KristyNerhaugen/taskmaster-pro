50e5cb2e940bca4059404c0c9da5f8eb082d4e85
kristynerhaugen <kristynerhaugen@gmail.com>
2022-05-24 16:55:04 -0500

    updated to allow users to edit task descriptions

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -45,8 +45,50 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+// event and callback function for <p>
+$(".list-group").on("click", "p", function() {
+  // text declaration
+  var text = $(this)
+  .text()
+  .trim();
+  // textInput declaration
+  var textInput = $("<textarea>")
+  .addClass("form-control")
+  .val(text);
+  // swap exisiting <p> with element with the new <textarea>
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
 
+// event listener, blur callback
+$(".list-group").on("blur", "textarea", function() {
+  // get the textarea's current value/text
+  var text = $(this)
+    .val()
+    .trim();
 
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
+
+    // recreate p element
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
